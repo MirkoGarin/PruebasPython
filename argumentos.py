3d853cd76b940739904b49bb409156a26288ccc2
Mirko Garin <mirkogarin@gmail.com>
2019-09-11 14:24:28 -0300

    Funciones
    Signed-off-by: Mirko Garin <mirkogarin@gmail.com>

---
+++ argumentos.py
@@ -0,0 +1,12 @@
+#!c:/Python/python.exe
+# -*- coding: utf-8 -*-  
+def saludo(nombreR, edad):
+    print ("Hola {} tienes {}" . format(, edad))
+
+def main():
+    nombre = input("Nombre: ")
+    edad = int(input("Edad: "))
+    saludo(nombre2, edad)
+
+if __name__ == '__main__':
+       main()   
