ba64fe0bd3b15debf8228ea89ca49b7cac8bbcbe
Mirko Garin <mirkogarin@gmail.com>
2019-09-12 11:42:03 -0300

    Update argumentos.py

--- argumentos.py
+++ argumentos.py
@@ -1,12 +1,12 @@
 #!c:/Python/python.exe
 # -*- coding: utf-8 -*-  
 def saludo(nombreR, edad):
-    print ("Hola {} tienes {}" . format(, edad))
+    print ("Hola {} tienes {}" . format(nombreR,edad))
 
 def main():
     nombre = input("Nombre: ")
     edad = int(input("Edad: "))
-    saludo(nombre2, edad)
+    saludo(nombre, edad)
 
 if __name__ == '__main__':
-       main()   
+       main()  
